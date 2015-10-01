fc6f766ce400624abf2ecf95960657f021c21430
admin <admin@macminissu4.training.local>
2015-10-01 12:49:28 +0300

    changes

---
+++ ecomap/ecomap/AddProblem/Views/AddProblemSolutionView.h
@@ -0,0 +1,13 @@
+//
+//  AddProblemSolutionView.h
+//  ecomap
+//
+//  Created by admin on 9/30/15.
+//  Copyright © 2015 SoftServe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddProblemSolutionView : UIView
+
+@end
---
+++ ecomap/ecomap/AddProblem/Views/AddProblemSolutionView.m
@@ -0,0 +1,18 @@
+//
+//  AddProblemSolutionView.m
+//  ecomap
+//
+//  Created by admin on 9/30/15.
+//  Copyright © 2015 SoftServe. All rights reserved.
+//
+
+#import "AddProblemSolutionView.h"
+
+@implementation AddProblemSolutionView
+
+//- (id)initWithCoder:(NSCoder *)aDecoder{
+   // self = [super initWithCoder:aDecoder];
+
+//}
+
+@end
